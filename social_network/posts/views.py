255b1fedaa9d7f95869b3e1715ad46de354bd1d5
leonid1704 <leonid1704@gmail.com>
2022-04-28 15:36:26 +0200

    add static, add names for urls

--- social_network/posts/views.py
+++ social_network/posts/views.py
@@ -3,9 +3,17 @@ from django.shortcuts import render
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    title = 'Последние обновления на сайте'
+    context = {
+        'title': title,
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
-    return render(request, template)
+    template = 'posts/group_posts.html'
+    title = 'Лев Толстой – зеркало русской революции.'
+    context = {
+        'title': title
+    }
+    return render(request, template, context)
