b5244610eaba93d3d7939613306ea07481cf64b8
leonid1704 <leonid1704@gmail.com>
2022-04-29 13:52:33 +0200

    add Post model and Group model

--- social_network/posts/views.py
+++ social_network/posts/views.py
@@ -1,19 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post, Group
 
 
 def index(request):
-    template = 'posts/index.html'
     title = 'Последние обновления на сайте'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
         'title': title,
+        'posts': posts
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
-    template = 'posts/group_posts.html'
-    title = 'Лев Толстой – зеркало русской революции.'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'title': title
+        'group': group,
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_posts.html', context)
