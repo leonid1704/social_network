e670750b4ab9be2cbd9a2993bd67553c1a409cca
leonid1704 <leonid1704@gmail.com>
2022-05-10 11:39:21 +0200

    add paginator

--- social_network/posts/views.py
+++ social_network/posts/views.py
@@ -1,23 +1,28 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
 from .models import Post, Group
 
 
 def index(request):
-    title = 'Последние обновления на сайте'
-    posts = Post.objects.order_by('-pub_date')[:10]
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'title': title,
-        'posts': posts
+        'page_obj': page_obj,
     }
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    post_list = Post.objects.filter(group=group)
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         'group': group,
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/group_posts.html', context)
