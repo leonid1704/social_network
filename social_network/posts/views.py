717a6b2749bdf9b2b53c2389c52294eaa921e7b0
leonid1704 <leonid1704@gmail.com>
2022-05-13 13:05:30 +0200

    add profile and post_detail

--- social_network/posts/views.py
+++ social_network/posts/views.py
@@ -1,7 +1,7 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
-from .models import Post, Group
+from .models import Post, Group, User
 
 
 def index(request):
@@ -26,3 +26,36 @@ def group_posts(request, slug):
         'page_obj': page_obj,
     }
     return render(request, 'posts/group_posts.html', context)
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    author_posts = Post.objects.filter(author=author)
+    paginator = Paginator(author_posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'author': author,
+        'paginator': paginator,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    author_posts = Post.objects.filter(author=post.author)
+    title = post.text[:30]
+    context = {
+        'post': post,
+        'author_posts': author_posts,
+        'title': title,
+    }
+    return render(request, 'posts/post_detail.html', context)
+
+
+
+
+
+#TODO Разобраться с профайлом и пост деталями. Можно красивее!
+#TODO Разобраться с тэмплейтами. Можно красивее!
